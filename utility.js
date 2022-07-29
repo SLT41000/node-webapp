fd03e59a8fa2ecc5820110e1f39565971e9629fc
ford41000 <42109410+ford41000@users.noreply.github.com>
2022-07-29 19:18:01 +0700

    codelab5

--- utility.js
+++ utility.js
@@ -3,7 +3,7 @@
 const { query } = require("express");
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4OTk4OTE4LCJleHAiOjE2NTkwMDI1MTgsIm5iZiI6MTY1ODk5ODkxOCwianRpIjoiUGJrMk9vRTJLZ2d4a2xCYSIsInN1YiI6MjcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.h5Bqk0aBoa1thKBq2Iedua574qF4A4LeVPdVPOX3wWE"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MDk2MjEzLCJleHAiOjE2NTkwOTk4MTMsIm5iZiI6MTY1OTA5NjIxMywianRpIjoiZnpxTzRMRExuQXZIZWtFQyIsInN1YiI6MjcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.UahTo96t6RgR3bImcLQSxfQ8_o-r-48nUZW7vxsIjw0"; // put access token
 const student = {
   student_id: 63364772,
   name: '', // replace with your full name.
